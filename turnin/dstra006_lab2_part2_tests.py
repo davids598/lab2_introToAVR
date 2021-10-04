a15b34a4c79d53d61936dded49d562d8e814d5ba
David Strathman <davidstrathman@gmail.com>
2021-10-03 20:58:52 -0700

    Finished Part 3

--- turnin/dstra006_lab2_part2_tests.py
+++ turnin/dstra006_lab2_part2_tests.py
@@ -17,7 +17,7 @@
 
 tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x004)],
+    'expected': [('PORTC',0x04)],
     },
     {'description': 'PINA: 0x02 => PORTC: 0x02',
     'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
